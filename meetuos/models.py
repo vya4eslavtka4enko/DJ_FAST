ccb037df6d03dd5833afd7dd3c85be01cd656fc2
Viacheslav <vya4eslav.tka4enko@gmail.com>
2024-02-01 23:40:51 +0000

    works

--- meetuos/models.py
+++ meetuos/models.py
@@ -19,13 +19,13 @@ class Participent(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length = 200)
-    organizer_email = models.EmailField(default = "email@gmail.com")
+    organizer_email = models.EmailField()
     date = models.DateField(default='2021-04-12')
     slug = models.SlugField(unique = True)
     description = models.TextField()
     image = models.ImageField(upload_to="images")
     location = models.ForeignKey(Location,on_delete = models.CASCADE,default="New York")
-    participent = models.ManyToManyField(Participent,blank = True,null=True)
+    participent = models.ManyToManyField(Participent,blank = True)
     def __str__(self):
         return f"{self.title} - {self.slug}"
     
